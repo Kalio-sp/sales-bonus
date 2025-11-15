576148c01d4e245c31b94e7a25ca8a64ec5ca319
Andru <andreaszerkalov@yandex.ru>
2025-11-15 19:59:51 +0500

    eight-change

--- src/main.js
+++ src/main.js
@@ -119,10 +119,10 @@ function analyzeSalesData(data, options) {
       // Revenue округляется на каждом шаге через calculateRevenue
       const revenue = calculateRevenue(item, product);
       const cost = product.purchase_price * item.quantity;
-      // ВАЖНО: округляем profit на каждом шаге!
-      const profit = +(revenue - cost).toFixed(2);
+      // УБИРАЕМ округление прибыли на каждом шаге!
+      const profit = revenue - cost;
 
-      // Суммируем с уже округленными значениями
+      // Суммируем точные значения без промежуточного округления
       seller.revenue += revenue;
       seller.profit += profit;
 
